daffdc64958fb385e62a9099a87639a2c5f7963f
Himanshu Singhal <himanshu17113@gmail.com>
2025-05-27 23:57:53 +0530

    start

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,59 @@
+// // Scripts for firebase and firebase messaging
+// importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+
+// // Initialize the Firebase app in the service worker by passing the generated config
+// var firebaseConfig = {
+//   apiKey: "YOUR_API_KEY",
+// //   authDomain: "YOUR_AUTH_DOMAIN",
+//   projectId: "bold-1d39b",
+// //   storageBucket: "YOUR_STORAGE_BUCKET",
+// //   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+//   appId: "1:593584096267:android:12aff36905f3a2d6b2730e"
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// // Retrieve firebase messaging
+// const messaging = firebase.messaging();
+
+// messaging.onBackgroundMessage(function(payload) {
+//   console.log('Received background message ', payload);
+
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//   };
+
+//   self.registration.showNotification(notificationTitle,
+//     notificationOptions);
+// });
+// Import the functions you need from the SDKs you need
+
+
+// https://github.com/firebase/flutterfire/blob/master/packages/firebase_core/firebase_core_web/lib/src/firebase_sdk_version.dart
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBTjWye2ChHTGsKhaVl9pkNFszW_MDGQnM",
+  authDomain: "bold-1d39b.firebaseapp.com",
+  projectId: "bold-1d39b",
+  storageBucket: "bold-1d39b.firebasestorage.app",
+  messagingSenderId: "593584096267",
+  appId: "1:593584096267:web:21da7e7fd08df104b2730e",
+  measurementId: "G-SMXVSX5WCJ"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((message) => {
+  console.log("onBackgroundMessage", message);
+});
